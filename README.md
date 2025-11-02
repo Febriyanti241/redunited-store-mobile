c62db46da4658faefc8fe6d4aec2eaa45b12b0ba
febriyanti241 <pebriy728@gmail.com>
2025-11-02 20:45:41 +0700

    Tugas Individu 7

--- README.md
+++ README.md
@@ -14,3 +14,24 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+
+
+TUGAS INDIVIDU 7
+1. Jelaskan apa itu widget tree pada Flutter dan bagaimana hubungan parent-child (induk-anak) bekerja antar widget.
+Jawab: Pada Flutter, seluruh tampilan aplikasi dibangun menggunakan widget, dan widget-widget tersebut tersusun dalam sebuah widget tree. Widget tree adalah struktur hierarki yang menggambarkan hubungan induk-anak antar widget. Setiap widget berada di dalam widget lainnya, sehingga parent menentukan tata letak dan aturan tampilannya, sedangkan child menampilkan konten di dalamnya. Misalnya, di aplikasi ini MaterialApp menjadi root widget, kemudian diikuti Scaffold yang membungkus AppBar serta SafeArea, lalu Column yang memuat widget-widget lain seperti Row, InfoCard, dan GridView yang berisi ItemCard. Hubungan ini membuat UI terstruktur dan mudah dikontrol.
+
+2. Sebutkan semua widget yang kamu gunakan dalam proyek ini dan jelaskan fungsinya.
+Jawab: Pada proyek ini terdapat berbagai widget penting. MaterialApp digunakan sebagai root aplikasi untuk menyediakan tema dan struktur dasar UI Material Design. Scaffold digunakan untuk menyediakan kerangka halaman dengan AppBar dan body. SafeArea melindungi konten agar tidak tertutup notch atau status bar. Layout diatur dengan Column, Row, Expanded, dan Padding, sedangkan Card dan Container digunakan untuk menampilkan informasi dalam bentuk kartu. GridView.count dipakai untuk menampilkan daftar menu berbentuk grid. Selain itu ada Text, Icon, Material, dan InkWell untuk menampilkan ikon, teks, dan memberikan efek sentuhan. Ketika card ditekan, SnackBar digunakan untuk menampilkan notifikasi kecil di bagian bawah layar.
+
+3. Apa fungsi dari widget MaterialApp? Jelaskan mengapa widget ini sering digunakan sebagai widget root
+Jawab: Widget MaterialApp memiliki peran penting karena digunakan sebagai widget root dan sering menjadi fondasi aplikasi Flutter. Widget ini mengatur tema, routing halaman, serta memastikan widget seperti Scaffold dan AppBar dapat berjalan sesuai desain Material Design. Karena itulah MaterialApp hampir selalu ditempatkan di bagian paling atas aplikasi.
+
+4. Jelaskan perbedaan antara StatelessWidget dan StatefulWidget. Kapan kamu memilih salah satunya?
+Jawab: Flutter memiliki dua tipe widget utama, yaitu StatelessWidget dan StatefulWidget. StatelessWidget digunakan ketika tampilan tidak memerlukan perubahan state selama aplikasi berjalan, dan kontennya hanya berdasarkan input awal. Sebaliknya, StatefulWidget digunakan ketika tampilan dapat berubah secara dinamis karena interaksi pengguna atau perubahan data. Dalam project ini, widget seperti MyHomePage, InfoCard, dan ItemCard dibuat sebagai StatelessWidget karena seluruh datanya statis dan tidak mengalami perubahan setelah ditampilkan. Jika suatu saat diperlukan perubahan data seperti counter, form, atau daftar produk dinamis, maka StatefulWidget lebih tepat digunakan.
+
+5. Apa itu BuildContext dan mengapa penting di Flutter? Bagaimana penggunaannya di metode build?
+Jawab: BuildContext merupakan objek penting di Flutter yang memberikan informasi tentang posisi widget di dalam widget tree dan memungkinkan widget untuk mengakses resource dari ancestor-nya seperti tema, ukuran layar, navigator, atau Scaffold. Pada kode ini, BuildContext digunakan antara lain untuk mengakses tema aplikasi melalui Theme.of(context) dan untuk menampilkan snackbar lewat ScaffoldMessenger.of(context). Tanpa BuildContext, widget tidak bisa berkomunikasi dengan elemen UI lainnya dalam tree.
+
+6. Jelaskan konsep "hot reload" di Flutter dan bagaimana bedanya dengan "hot restart".
+Jawab: Flutter memiliki fitur hot reload dan hot restart untuk mempercepat pengembangan aplikasi. Hot reload memungkinkan perubahan kode segera terlihat tanpa menghapus state aplikasi. Ini sangat berguna ketika melakukan perubahan UI karena aplikasi tidak perlu dimulai ulang. Sedangkan hot restart akan memulai ulang aplikasi dari awal dan menghapus state yang sedang berjalan, sehingga digunakan ketika perubahan kode yang dibuat memengaruhi struktur atau inisialisasi aplikasi. Perbedaan ini membantu pengembang bekerja lebih efisien dalam proses development. Contoh kasusnya adalah ketika ingin mengubah warna/teks maka gunakan Hot Reload, dan jika ingin menambah variabel global baru maka gunakan Hot Start.
---
+++ lib/menu.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({super.key});
+
+  final String nama = "Febriyanti"; //nama
+  final String npm = "2406496214"; //npm
+  final String kelas = "A"; //kelas
+
+  final List<ItemHomepage> items = [
+    ItemHomepage("All Product", Icons.newspaper, Colors.blue),
+    ItemHomepage("My Product", Icons.newspaper, Colors.green),
+    ItemHomepage("Create Product", Icons.add, Colors.red),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    // Scaffold menyediakan struktur dasar halaman dengan AppBar dan body.
+    return Scaffold(
+      // AppBar adalah bagian atas halaman yang menampilkan judul.
+      appBar: AppBar(
+        title: const Text(
+          'RedUnited Store',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        // Warna latar belakang AppBar diambil dari skema warna tema aplikasi.
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+
+      // Gunakan SafeArea supaya tidak tertutup status bar / notch.
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          // Menyusun widget secara vertikal dalam sebuah kolom.
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Row untuk menampilkan 3 InfoCard secara horizontal.
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  InfoCard(title: 'NPM', content: npm),
+                  InfoCard(title: 'Name', content: nama),
+                  InfoCard(title: 'Class', content: kelas),
+                ],
+              ),
+
+              // Memberikan jarak vertikal 16 unit.
+              const SizedBox(height: 16.0),
+
+              // Teks sambutan
+              const Padding(
+                padding: EdgeInsets.only(top: 16.0),
+                child: Text(
+                  'Selamat datang di RedUnited Store',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 12.0),
+
+              // Expanded agar GridView mengambil ruang sisa dan bisa menggulir sendiri.
+              Expanded(
+                child: GridView.count(
+                  // Jangan pakai primary: true atau shrinkWrap: true di sini.
+                  padding: const EdgeInsets.all(20),
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  crossAxisCount: 3,
+                  children: items.map((ItemHomepage item) {
+                    return ItemCard(item);
+                  }).toList(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class InfoCard extends StatelessWidget {
+  // Kartu informasi yang menampilkan title dan content.
+  final String title; // Judul kartu.
+  final String content; // Isi kartu.
+
+  const InfoCard({super.key, required this.title, required this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      // Membuat kotak kartu dengan bayangan dibawahnya.
+      elevation: 2.0,
+      child: Container(
+        // Mengatur ukuran dan jarak di dalam kartu.
+        width: MediaQuery.of(context).size.width / 3.5, // menyesuaikan dengan lebar device yang digunakan.
+        padding: const EdgeInsets.all(16.0),
+        // Menyusun title dan content secara vertikal.
+        child: Column(
+          children: [
+            Text(
+              title,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8.0),
+            Text(content),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemHomepage {
+  final String name;
+  final IconData icon;
+  final Color color;
+
+  ItemHomepage(this.name, this.icon, this.color);
+}
+
+class ItemCard extends StatelessWidget {
+  // Menampilkan kartu dengan ikon dan nama.
+
+  final ItemHomepage item;
+
+  const ItemCard(this.item, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      // Menentukan warna latar belakang dari item.
+      color: item.color,
+      // Membuat sudut kartu melengkung.
+      borderRadius: BorderRadius.circular(12),
+
+      child: InkWell(
+        borderRadius: BorderRadius.circular(12),
+        // Aksi ketika kartu ditekan.
+        onTap: () {
+          // Menampilkan pesan SnackBar saat kartu ditekan.
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              SnackBar(content: Text("Kamu telah menekan tombol ${item.name}!")),
+            );
+        },
+        // Container untuk menyimpan Icon dan Text
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Column(
+              // Menyusun ikon dan teks di tengah kartu.
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  item.icon,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                const Padding(padding: EdgeInsets.all(3)),
+                Text(
+                  item.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
